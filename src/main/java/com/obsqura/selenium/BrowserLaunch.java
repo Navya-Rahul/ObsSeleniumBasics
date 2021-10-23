9860a737e670cd41fff97e2a26da48a889f70564
user <navyanaveenam@gmail.com>
2021-10-23 22:35:37 +0530

    Third Commit

--- src/main/java/com/obsqura/selenium/BrowserLaunch.java
+++ src/main/java/com/obsqura/selenium/BrowserLaunch.java
@@ -1,6 +1,8 @@
 package com.obsqura.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -22,7 +24,7 @@ public class BrowserLaunch {
         }
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.get("http://demo.guru99.com/test/newtours/");
+        driver.get("http://demowebshop.tricentis.com/");
     }
     public static void main(String[] args) {
         BrowserLaunch.testInitialize("chrome");
@@ -31,7 +33,33 @@ public class BrowserLaunch {
         String url = driver.getCurrentUrl();
         System.out.println(url);
         String source = driver.getPageSource();
-        System.out.println(source);
-        driver.close();
+        //System.out.println(source);
+        WebElement loginMenu = driver.findElement(By.className("ico-login"));
+        String loginText = loginMenu.getText();
+        System.out.println(loginText);
+        loginMenu.click();
+        WebElement userName = driver.findElement(By.id("Email"));
+        userName.sendKeys("navya@gmail.com");
+        userName.clear();
+        String classValue = userName.getAttribute("class");
+        System.out.println(classValue);
+        String tagName = userName.getTagName();
+        System.out.println(tagName);
+        WebElement password = driver.findElement(By.id("Password"));
+        //password.sendKeys("rithul123");
+        WebElement submit = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input"));
+       // submit.click();
+        //driver.close();
+        //WEB ELEMENTS
+       /* WebElement userName = driver.findElement(By.id("Email"));
+        userName.sendKeys("navya@gmail.com");
+        userName.click();
+        WebElement name = driver.findElement(By.name("Email"));
+        WebElement className = driver.findElement(By.className("email"));
+        WebElement xpath = driver.findElement(By.xpath("//*[@id=\"Email\"]"));
+        WebElement cssSelector = driver.findElement(By.cssSelector("#Email"));
+        WebElement tagName = driver.findElement(By.tagName("input"));
+        WebElement linkText = driver.findElement(By.linkText("Log in"));
+        WebElement partialLinkText = driver.findElement(By.partialLinkText("Log"));*/
     }
 }
