c6f02203b3854b4565f021404caca0f7b275ef79
user <navyanaveenam@gmail.com>
2021-10-29 23:42:39 +0530

    Sixth Commit

--- src/test/java/com/obsqura/commands/Commands.java
+++ src/test/java/com/obsqura/commands/Commands.java
@@ -1,9 +1,12 @@
 package com.obsqura.commands;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,7 +28,6 @@ public class Commands {
         }
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.get("http://demowebshop.tricentis.com/");
     }
     @BeforeMethod
     public void setup()
@@ -35,18 +37,44 @@ public class Commands {
     @AfterMethod
     public void tearDown()
     {
-        driver.close();
+        //driver.close();
     }
     @Test(priority = 1,enabled = false)
     public void verifyUserLogin()
     {
-        System.out.println("testNGexample");
+        driver.get("http://demowebshop.tricentis.com/");
+        WebElement login = driver.findElement(By.xpath("//a[@class='ico-login']"));
+        login.click();
+        WebElement userName = driver.findElement(By.id("Email"));
+        userName.sendKeys("navyanaveenam@gmail.com");
+        WebElement password = driver.findElement(By.id("Password"));
+        password.sendKeys("Rithul@12");
+        WebElement submit = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+        submit.click();
+        WebElement loginText = driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
+        String actualUserName = loginText.getText();
+        String expectedUsername = "navyanaveenam@gmail.com";
+        Assert.assertEquals(actualUserName,expectedUsername,"LOGIN FAILED");
+
     }
-    @Test(priority = 2)
+    @Test(priority = 2,enabled = false)
     public void verifyHomePageTitle()
     {
-        String title = driver.getTitle();
-        System.out.println(title);
+        driver.get("http://demowebshop.tricentis.com/");
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Demo Web Shop";
+        Assert.assertEquals(actualTitle,expectedTitle,"INVALID HOMEPAGE TITLE");
+    }
+    @Test(priority = 3,enabled = true)
+    public void verifyFileUpload()
+    {
+        driver.get("https://demo.guru99.com/test/upload/");
+        WebElement chooseFile = driver.findElement(By.id("uploadfile_0"));
+        chooseFile.sendKeys("C:\\selenium\\data\\sample.txt");
+        WebElement accept = driver.findElement(By.id("terms"));
+        accept.click();
+        WebElement submit = driver.findElement(By.id("submitbutton"));
+        submit.click();
     }
 }
 
